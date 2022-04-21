520114b5a1f01ae778c3de6da7b31d0f920cd0eb
Avdhesh <avdhesh220@gmail.com>
2022-04-21 12:59:27 +0530

    added new changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,13 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
+
+
+
+
+     public static void main() {
         System.out.println("Hello world!");
         System.out.println("Hello world again");
-
-
-
 
     }
 }
---
+++ src/test/java/Test1.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class Test1
+{
+    @Test
+    public void method(){
+        System.out.print("This is our first test");
+    }
+}
